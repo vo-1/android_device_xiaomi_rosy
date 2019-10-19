680b07874f35eb926eb3f40eeb5db53fc81c98ae
Vladimir Chernov <vo-1@yandex.ru>
2019-10-20 02:32:23 +0300

    Build Snap

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Camera2
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
